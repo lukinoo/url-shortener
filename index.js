f1e739064b12de61b051b385c4128fae4ef4a055
lukinoo <luka.bliadze.2015@mail.ru>
2022-06-11 22:51:15 +0400

    feature/added-less-styles

--- index.js
+++ index.js
@@ -4,20 +4,14 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const ShortUrl = require("./models/schema");
 
 const app = require("liquid-express-views")(express());
 
-mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("mongodb connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +23,12 @@ app.get("/", async (req, res) => {
   res.render("index", {
     title: "URL shortener",
   });
+});
+
+app.post("/short-url", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullURl });
+
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT, () => {
